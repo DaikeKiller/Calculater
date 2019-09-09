80879be2c5df8cf24e7990959efbcf3c1be0b6b0
MKC <mengkun.chen@duke.edu>
2019-09-09 12:23:38 -0400

    add HDL

--- calculator.py
+++ calculator.py
@@ -1,8 +1,15 @@
+def checkHDL(HDL_result):
+	if HDL_result >= 60:
+		return "Normal"
+	elif 40 <= HDL <60:
+		return "Borderline low"
+	else:
+		return "low"
+
 def interface():
 	print('My calculator program')
 	keep_running == True
-	while keep_running:
-		
+	while keep_running:		
 		print('Options:')
 		print('9 - Quit')
 		choice = input('Enter your choice:')
